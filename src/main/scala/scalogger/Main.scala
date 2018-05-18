1f5ca7aa6f5a394fc5eda4178d40e354df6542f8
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-05-18 00:02:19 -0300

    feat: add ScalaFX

--- src/main/scala/scalogger/Main.scala
+++ src/main/scala/scalogger/Main.scala
@@ -1,5 +1,24 @@
 package scalogger
 
-object Main extends App {
-  println("Welcome to Scalogger!")
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color._
+import scalafx.scene.text.Text
+
+object Main extends JFXApp {
+  stage = new JFXApp.PrimaryStage()
+  stage.title.value = "Scalogger"
+
+  val scene = new Scene()
+  scene.fill = Black
+  val content = new HBox()
+  val screenText = new Text()
+  screenText.text = "Welcome to Scalogger!"
+  screenText.fill = LightGreen
+  screenText.style = "-fx-font-size: 48pt"
+  content.children = screenText
+  scene.content = content
+
+  stage.scene = scene
 }
