0541ab0446926bbd3db596398ddeab2caf7411ba
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-05-20 06:52:41 -0300

    refactor: make properties private

--- src/main/scala/scalogger/engine/GameController.scala
+++ src/main/scala/scalogger/engine/GameController.scala
@@ -8,11 +8,10 @@ import scala.collection.mutable
 
 object GameController {
 
-  val gameEntities = new mutable.MutableList[GameEntity]()
+  private val gameEntities = new mutable.MutableList[GameEntity]()
+  private val gridSize = 16
 
-  val gridSize = 16
-
-  var lastTime: Long = 0
+  private var lastTime: Long = 0
 
   def initialize(pane: Pane): Unit = {
     gameEntities += new Frog(new Vector2(7.5 * gridSize, 14.5 * gridSize), 0.2, gridSize)
