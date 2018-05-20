3470d00469cba102db558229387d3614b7cc3d88
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-05-20 06:37:44 -0300

    fix: use milliseconds for delta time

--- src/main/scala/scalogger/engine/GameController.scala
+++ src/main/scala/scalogger/engine/GameController.scala
@@ -15,7 +15,7 @@ object GameController {
   var lastTime: Long = 0
 
   def initialize(pane: Pane): Unit = {
-    gameEntities += new Frog(new Vector2(7.5 * gridSize, 14.5 * gridSize), 1, gridSize)
+    gameEntities += new Frog(new Vector2(7.5 * gridSize, 14.5 * gridSize), 0.2, gridSize)
 
     for (entity <- gameEntities) {
       pane.getChildren.add(entity.getImageView)
@@ -25,19 +25,20 @@ object GameController {
   def run(): Unit = {
     val animator = new AnimationTimer() {
       override def handle(now: Long): Unit = {
+        val nowMilliSec = now / 1000000
         if (lastTime == 0) {
-          lastTime = now
+          lastTime = nowMilliSec
         }
         for (entity <- gameEntities) {
           entity.readInputs()
         }
         for (entity <- gameEntities) {
-          entity.computePhysics(now - lastTime)
+          entity.computePhysics(nowMilliSec - lastTime)
         }
         for (entity <- gameEntities) {
           entity.render()
         }
-        lastTime = now
+        lastTime = nowMilliSec
       }
     }
     animator.start()
