c22235a8607035d0d81b42d6cc028093ad46bdf0
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-06-18 20:40:58 -0300

    refactor: move constants out of initialization

--- objectoriented/src/main/scala/scalogger/engine/GameController.scala
+++ objectoriented/src/main/scala/scalogger/engine/GameController.scala
@@ -13,22 +13,24 @@ import scala.collection.mutable
 object GameController {
 
   private val gameEntities = new mutable.ListBuffer[GameEntity]()
+
+  private val gridSize = 16
   private val gridWidth = 14
+
   private var gameScreen: Pane = _
 
   private var score: Long = 0
 
   def initialize(pane: Pane): Unit = {
     gameScreen = pane
-    val gridSize = 16
-    val gridArea = new Box(0, 2, gridWidth, 13) * gridSize
     val scoreText = new Text()
 
-    this.initScoreText(scoreText, gridSize)
+    this.initScoreText(scoreText)
     pane.getChildren.add(scoreText)
 
-    renderMap(gridSize)
+    renderMap()
 
+    val gridArea = new Box(0, 2, gridWidth, 13) * gridSize
     addGameEntity(new Frog(new Vector2(7.5 * gridSize, 14.5 * gridSize), 0.2, gridSize, gridArea))
   }
 
@@ -36,7 +38,7 @@ object GameController {
     score += value
   }
 
-  private def initScoreText(scoreText: Text, gridSize: Int): Unit = {
+  private def initScoreText(scoreText: Text): Unit = {
     scoreText.setText("SCORE: " + score.toString)
     scoreText.setFont(Font.font("Verdana", 20))
     scoreText.setFill(Color.WHITE)
@@ -44,13 +46,13 @@ object GameController {
     scoreText.setY(1 * gridSize)
   }
 
-  private def renderMap(gridSize: Double): Unit = {
+  private def renderMap(): Unit = {
     val water = (new Box(0, 1.2, gridWidth, 7) * gridSize).toRectangle
     water.setFill(Color.DARKBLUE)
     gameScreen.getChildren.add(water)
 
-    this.renderSideWalk(gridSize, 8)
-    this.renderSideWalk(gridSize, 14)
+    this.renderSideWalk(8)
+    this.renderSideWalk(14)
 
     val finalView = new ImageView(Sprite.FINAL)
     finalView.setFitWidth(gridSize * gridWidth)
@@ -60,7 +62,7 @@ object GameController {
     gameScreen.getChildren.add(finalView)
   }
 
-  private def renderSideWalk(gridSize: Double, lane: Double): Unit = {
+  private def renderSideWalk(lane: Double): Unit = {
     for (i <- 0 until gridWidth) {
       val sideWalkView = new ImageView(Sprite.SIDEWALK)
       sideWalkView.setFitWidth(gridSize)
