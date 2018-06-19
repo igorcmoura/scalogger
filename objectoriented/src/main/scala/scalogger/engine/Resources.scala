972388de4ae1a55ce989bc250ebb21e1c087eddf
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-06-19 00:10:46 -0300

    feat: add car resources

--- objectoriented/src/main/scala/scalogger/engine/Resources.scala
+++ objectoriented/src/main/scala/scalogger/engine/Resources.scala
@@ -9,6 +9,12 @@ object Resources {
     val FROG_IDLE = new Image("file:sprites/frog_idle.png")
     val FROG_JUMPING = new Image("file:sprites/frog_jumping.png")
 
+    val CAR = new Image("file:sprites/car.png")
+    val BULLDOZER = new Image("file:sprites/bulldozer.png")
+    val SPORTCAR_YELLOW = new Image("file:sprites/sportcar_yellow.png")
+    val SPORTCAR_GREY = new Image("file:sprites/sportcar_grey.png")
+    val TRUCK = new Image("file:sprites/truck.png")
+
     val SIDEWALK = new Image("file:sprites/sidewalk.png")
     val FINAL = new Image("file:sprites/final.png")
   }
