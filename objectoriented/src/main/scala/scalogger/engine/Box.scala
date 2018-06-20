5f00959817a16dd6511fa5846081f45e4bf980b2
Igor Cescon de Moura <igor.moura@corp.globo.com>
2018-06-20 11:52:28 -0300

    fix: add new collision cases

--- objectoriented/src/main/scala/scalogger/engine/Box.scala
+++ objectoriented/src/main/scala/scalogger/engine/Box.scala
@@ -9,11 +9,15 @@ class Box(val x: Double, val y: Double, val width: Double, val height: Double) {
   def -(value: Double) = new Box(this.x + value, this.y + value, this.width - value * 2, this.height - value * 2)
   def *(value: Double) = new Box(this.x * value, this.y * value, this.width * value, this.height * value)
 
+  def middle = new Vector2(x + (width / 2), y + (height / 2))
+
   def collidesWith(other: Box): Boolean =
     new Vector2(x, y).isInside(other) ||
       new Vector2(x + width, y).isInside(other) ||
       new Vector2(x, y + height).isInside(other) ||
-      new Vector2(x + width, y + height).isInside(other)
+      new Vector2(x + width, y + height).isInside(other) ||
+      new Vector2(middle.x, middle.y).isInside(other) ||
+      new Vector2(other.middle.x, other.middle.y).isInside(this)
 
   def toRectangle = new Rectangle(this.x, this.y, this.width, this.height)
 }
