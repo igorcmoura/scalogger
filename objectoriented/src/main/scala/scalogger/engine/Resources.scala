7b52f7839c875d1a9f1289b3e4de554e4f7eedb6
Igor Cescon de Moura <igor.moura@corp.globo.com>
2018-06-20 12:42:35 -0300

    feat: add turtles resources

--- objectoriented/src/main/scala/scalogger/engine/Resources.scala
+++ objectoriented/src/main/scala/scalogger/engine/Resources.scala
@@ -16,6 +16,9 @@ object Resources {
     val TRUCK = new Image("file:sprites/truck.png")
 
     val LOG = new Image("file:sprites/log.png")
+    val TURTLE_1 = new Image("file:sprites/turtle_1.png")
+    val TURTLE_2 = new Image("file:sprites/turtle_2.png")
+    val TURTLE_3 = new Image("file:sprites/turtle_3.png")
 
     val SIDEWALK = new Image("file:sprites/sidewalk.png")
     val FINAL = new Image("file:sprites/final.png")
