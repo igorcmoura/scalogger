2c37dc98ccd1a031f22de01f2fdd525c290c699a
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-06-19 22:53:21 -0300

    fix: box collision method

--- objectoriented/src/main/scala/scalogger/engine/Box.scala
+++ objectoriented/src/main/scala/scalogger/engine/Box.scala
@@ -10,10 +10,10 @@ class Box(val x: Double, val y: Double, val width: Double, val height: Double) {
   def *(value: Double) = new Box(this.x * value, this.y * value, this.width * value, this.height * value)
 
   def collidesWith(other: Box): Boolean =
-    new Vector2(x, y).isInside(this) ||
-      new Vector2(x + width, y).isInside(this) ||
-      new Vector2(x, y + height).isInside(this) ||
-      new Vector2(x + width, y + height).isInside(this)
+    new Vector2(x, y).isInside(other) ||
+      new Vector2(x + width, y).isInside(other) ||
+      new Vector2(x, y + height).isInside(other) ||
+      new Vector2(x + width, y + height).isInside(other)
 
   def toRectangle = new Rectangle(this.x, this.y, this.width, this.height)
 }
