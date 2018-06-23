dd0af0c2c0902fa35c9ec117cf8701c6e216b732
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-06-23 16:43:06 -0300

    feat: create diver turtles

--- objectoriented/src/main/scala/scalogger/entities/Turtles.scala
+++ objectoriented/src/main/scala/scalogger/entities/Turtles.scala
@@ -10,20 +10,27 @@ import scalogger.managers.{GameController, GameMap}
 class Turtles(initialPosition: Vector2,
               private var size: Int,
               private var speed: Double,
+              diverChance: Double,
               private var spriteTime: Double,
               private var map: GameMap) extends Rideable with Spawnable[Turtles] {
   private var position = initialPosition
+
+  private val isDiver = math.random() < diverChance
 
   private val imageViews: List[ImageView] = List.fill(size)(new ImageView(Sprite.TURTLE_1))
   for (imageView <- imageViews) {
     imageView.setFitWidth(map.gridSize)
     imageView.setFitHeight(map.gridSize)
   }
-  private val images = Seq(Sprite.TURTLE_1, Sprite.TURTLE_2, Sprite.TURTLE_3)
+  private val images = if (isDiver) {
+    Seq(Sprite.TURTLE_DIVE_1, Sprite.TURTLE_2, Sprite.TURTLE_3, Sprite.TURTLE_DIVE_1, Sprite.TURTLE_DIVE_2, Sprite.VOID) // Void must be last
+  } else {
+    Seq(Sprite.TURTLE_1, Sprite.TURTLE_2, Sprite.TURTLE_3)
+  }
   private var currentImageIdx = 0
   private var nextImageTime = spriteTime
 
-  override def spawn(): Turtles = new Turtles(this.position, this.size, this.speed, this.spriteTime, this.map)
+  override def spawn(): Turtles = new Turtles(this.position, this.size, this.speed, this.diverChance, this.spriteTime, this.map)
 
   override def attachToScreen(screen: Pane): Unit = {
     for (imageView <- imageViews) {
@@ -47,6 +54,10 @@ class Turtles(initialPosition: Vector2,
       GameController.removeGameEntity(this)
     }
     nextImageTime -= deltaTime
+    if (isDiver && currentImageIdx == images.size - 1 && this.carrying != null) {
+      this.carrying.getOff()
+      this.drop(this.carrying)
+    }
   }
 
   override def render(): Unit = {
