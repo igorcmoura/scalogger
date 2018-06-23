d43015e75698e32d7788310a0d11a73558f51fbf
Igor Cescon de Moura <igorcmgamer@gmail.com>
2018-06-23 16:39:09 -0300

    refator: move image base on map grid for better readability

--- objectoriented/src/main/scala/scalogger/entities/Frog.scala
+++ objectoriented/src/main/scala/scalogger/entities/Frog.scala
@@ -189,8 +189,8 @@ class Frog(initialPosition: Vector2,
       }
     }
     imageView.toFront()
-    imageView.setX(position.x - stepDistance / 2)
-    imageView.setY(position.y - stepDistance / 2)
+    imageView.setX(position.x - map.gridSize / 2)
+    imageView.setY(position.y - map.gridSize / 2)
     facingDirection match {
       case UP => imageView.setRotate(0)
       case DOWN => imageView.setRotate(180)
